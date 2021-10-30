931db8116fdcb47cc2e986589a1b635f1c6515f6
nairo-rojas <86504357+nairo-rojas@users.noreply.github.com>
2021-10-29 22:16:26 -0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,31 +1,29 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env' });
+dotenv.config({path: './.env'}); 
 
 const stringConexion = process.env.DATABASE_URL;
 
 const client = new MongoClient(stringConexion, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
+const getDB = ()=>{
+    return baseDeDatos;
+}
 let baseDeDatos;
 
-const connectServer = (callback) => {
-  client.connect((err, db) => {
-    if (err) {
-      console.error('Error conectando a la base de datos');
-      return 'error';
-    }
-    baseDeDatos = db.db('Productos');
-    console.log('baseDeDatos exitosa');
-    return callback();
-  });
+const conectarDB =(callback)=>{
+    client.connect((err, db)=>{
+        if (err) {
+            console.error("error conectando a la base de datos")
+        } 
+        baseDeDatos = db.db('Gestion-ventas');
+        console.log("Conexión existosa a la baseDeDatos")
+        return callback();
+    });
 };
 
-const getDB = () => {
-  return baseDeDatos;
-};
-
-export { connectServer, getDB };
+export {conectarDB, getDB};
