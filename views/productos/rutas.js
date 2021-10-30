21700b64af0b34029920e31e6c3497472b7c9012
nairo-rojas <86504357+nairo-rojas@users.noreply.github.com>
2021-10-29 22:17:10 -0500

    Update rutas.js

--- views/productos/rutas.js
+++ views/productos/rutas.js
@@ -1,43 +1,39 @@
 import Express from 'express';
-import { getAllProducts, crearProducto, editarProducto, eliminarProducto,  } from '../../controllers/productos/controller.js';
+import { queryAllProducts, crearProducto, editarProducto, elimiarProducto, consultarProducto } from '../../controllers/productos/controller.js';
 
+const rutasProductos = Express.Router();
 
-
-const rutasProducto = Express.Router();
-
-const genericCallback = (res) => (err, result) => {
+const genericCallback = (res)=>(err, result)=>{
         if (err) {
-            console.log("error", err);
-            res.status(500).json({ error:err });
-        } else {
-            res.json(result);
-        }
-    };
+                console.log('error', err);
+                res.status(500).json({error: err})
+            } else {
+                res.json(result);
+            }
+        };
 
-
-rutasProducto.route('/productos').get((req, res) => {
-    console.log("Alguien hizo GET a todos los productos");
-    getAllProducts( genericCallback(res));
+rutasProductos.route('/productos').get((req,res)=>{
+    console.log("Alguien hizo get en la ruta /productos");
+    queryAllProducts(genericCallback(res));
+   
 });
 
-rutasProducto.route('/productos').post((req, res) => {
-    console.log("Alguien creó un producto")
+rutasProductos.route('/productos').post((req, res)=>{
     crearProducto(req.body, genericCallback(res));
 });
 
-// rutasProducto.route('/productos').get((req, res) => {
-//     console.log("Alguien hizo GET a un producto");
-//     consultarProducto(req.params.id, genericCallback(res));
-// });
+rutasProductos.route('/productos/:id').get((req,res)=>{
+    console.log("Alguien hizo get en la ruta /productos");
+    consultarProducto(req.params.id, genericCallback(res));
+   
+});
 
-rutasProducto.route('/productos/:id').patch((req, res) => {
-    console.log("Alguien editó un producto")
+rutasProductos.route('/productos/:id').patch((req, res)=>{
     editarProducto(req.params.id, req.body, genericCallback(res));
 });
 
-rutasProducto.route('/productos/:id').delete((req, res) => {
-    console.log("Alguien eliminó un producto")
-    eliminarProducto(req.params.id, genericCallback(res));
+rutasProductos.route('/productos/:id').delete((req, res)=>{
+    elimiarProducto(req.params.id, genericCallback(res))
 });
 
-export default rutasProducto;
+export default rutasProductos;
