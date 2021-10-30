f43fa5bce186ad8d0b7695b84b63043f7ec3c775
nairo-rojas <86504357+nairo-rojas@users.noreply.github.com>
2021-10-29 22:19:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,32 @@
-//forma antigua o tradicional de hacer improt
-//const require = require('express');
-//En package.js agregar "type": "modules" para hacer impor normales
 import Express from "express";
-import Cors from 'cors';
+import { ObjectId } from "mongodb";
+import { conectarDB } from "./db/db.js";
 import dotenv from 'dotenv';
-import { connectServer} from './db/db.js';
-import jwt from "express-jwt";
-import jwks from "jwks-rsa";
-import rutasProducto from "./views/productos/rutas.js";
-import rutasUsuario from "./views/usuarios/rutas.js";
+import Cors from 'cors';
+import rutasProductos from "./views/productos/rutas.js";
+import rutasUsuarios from "./views/usuarios/rutas.js";
 import rutasVentas from "./views/ventas/rutas.js";
+import autorizacionEstadoUsuario from "./middleware/autorizacionEstadoUsuario.js";
 
 
-dotenv.config({path:'./.env'});
+dotenv.config({path: './.env'});
 
 const app = Express();
+
 
 app.use(Express.json());
 app.use(Cors());
 
-var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://gestion-ventas-ants.us.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'api-autenticacion-gestion-ventas',
-  issuer: 'https://gestion-ventas-ants.us.auth0.com/',
-  algorithms: ['RS256']
-});
+app.use(autorizacionEstadoUsuario);
+app.use(rutasProductos);
+app.use(rutasUsuarios);
+app.use(rutasVentas);
 
-app.use(jwtCheck);
-app.use(rutasProducto);
-app.use(rutasUsuario)
-app.use(rutasVentas)
 
-const main = () => {
-    return app.listen(process.env.PORT, () => {
+const main = ()=>{
+    return app.listen(process.env.PORT, ()=>{
         console.log(`Escuchando en el puerto ${process.env.PORT}`);
     });
 };
 
-connectServer(main);
-
-
+conectarDB(main);
